8c05685115673e698299fca551be246694fdaf99
Andre Duarte <aguimaraesduarte@dons.usfca.edu>
2016-07-25 14:15:27 -0700

    sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -5,17 +5,26 @@ x_0 = 1
 x_{i+1} =0.5 * (x_i + n/x_i)
 '''
 import sys
+import numpy as np
 
 def sqrt(n):
-	PRECISION = 0.0000001
+	PRECISION = 0.000000001
 	x0 = 1.0
 	xold = x0
 	while True:
-		print "x_i = %f" % xold
 		xnew = 0.5 * (xold + n/xold)
 		if abs(xnew - xold) < PRECISION: break
 		xold = xnew
 	return xnew
 
-s = sqrt(int(sys.argv[1]))
-print "sqrt(%d) is %f" % (int(sys.argv[1]), s)
+def test_sqrt():
+	def check(n):
+		assert np.isclose(sqrt(n), np.sqrt(n))
+	check(87291)
+	check(100)
+	check(0)
+	check(1)
+	check(2)
+
+
+test_sqrt()
