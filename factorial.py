2af465b5efad720092fa711436f677061e01f780
Andre Duarte <aguimaraesduarte@dons.usfca.edu>
2016-07-25 13:28:22 -0700

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+import sys
+
+def factorial(n):
+	if n < 0:
+		print "%d is negative! Can't do that!" % n
+		sys.exit(0)
+	if n == 0: return 1
+	return n*factorial(n-1)
+
+print factorial(int(sys.argv[1]))
